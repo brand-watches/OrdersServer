411904e2ff7b41e12d4612e0a34619053d8f3ea7
eviv2206 <zhenya.saprin@mail.ru>
2023-10-09 21:44:06 +0300

    first commit

---
+++ src/main/java/by/brandwatch/orderssevice/OrdersSeviceApplication.java
@@ -0,0 +1,13 @@
+package by.brandwatch.orderssevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersSeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/brandwatch/orderssevice/OrdersSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.brandwatch.orderssevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
